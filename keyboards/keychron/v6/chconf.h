085c1cc6b2206938f714da860904017e70f295d9
cxen <c_x_@outlook.com>
2023-08-20 13:42:42 +0200

    Mods over Vial firmware for Keychron V6 ISO+encoder
        Modified Keychron V6 firmware and Vial Keymap to:
        Enable NUM_LOCK backlight
        Enable all RGB modes
        Add DEBOUNCE_TYPE = sym_eager_pk
        Increase scanning frequency to 50000 (from 10000)

---
+++ keyboards/keychron/v6/chconf.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define CH_CFG_ST_FREQUENCY 50000
+
+#include_next <chconf.h>
--- keyboards/keychron/v6/iso_encoder/keymaps/vial/rules.mk
+++ keyboards/keychron/v6/iso_encoder/keymaps/vial/rules.mk
@@ -2,6 +2,7 @@ VIA_ENABLE = yes
 VIAL_ENABLE = yes
 VIALRGB_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
+DEBOUNCE_TYPE = sym_eager_pk
 
 VPATH += keyboards/keychron/common
 SRC += keychron_common.c
